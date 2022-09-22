dd319915861ab87db1d33e570c6ce7eaa677c250
이건회 <rg970604@naver.com>
2022-09-22 16:03:51 +0900

    refactor StringAddCalculator

--- src/test/java/study/StringAddCalculator.java
+++ src/test/java/study/StringAddCalculator.java
@@ -4,28 +4,33 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringAddCalculator {
+    static int delimiternum = 1;
+    static int textnum = 2;
+
+    static String customPattern = "//(.)\n(.*)";
+
     public static int splitAndSum(String text) {
-        if (emptyOrNull(text)){
+        if (emptyOrNull(text)) {
             return 0;
         }
         if (singleNum(text)) {
             return Integer.parseInt(text);
         }
 
-        Matcher pattern = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher pattern = Pattern.compile(customPattern).matcher(text);
 
         int patternSum = 0;
-        int notPatternSum =0 ;
+        int notPatternSum = 0;
 
         if (isPatternMatched(pattern)) {
             patternSum = sumPattern(pattern);
         } else {
-            notPatternSum = sumAll(text);
+            notPatternSum = textIntoIntAndSumAll(text);
         }
-        if (patternSum >0){
-            return  patternSum;
+        if (patternSum > 0) {
+            return patternSum;
         }
-        if (notPatternSum>0){
+        if (notPatternSum > 0) {
             return notPatternSum;
         }
 
@@ -40,26 +45,24 @@ public class StringAddCalculator {
         return isEmptyOrNull;
     }
 
-    private static boolean singleNum(String text){
+    private static boolean singleNum(String text) {
         boolean isSingleNum = false;
-        if (text.length() == 1){
+        if (text.length() == 1) {
             isSingleNum = true;
         }
-        return isSingleNum;
+        return text.length() == 1;
     }
 
-    private static boolean isPatternMatched (Matcher pattern){
+    private static boolean isPatternMatched(Matcher pattern) {
         boolean isPattern = false;
-        if (pattern.find()){
+        if (pattern.find()) {
             isPattern = true;
         }
-        return  isPattern;
+        return isPattern;
 
     }
 
-    private static int sumPattern(Matcher pattern){
-        int delimiternum = 1;
-        int textnum = 2;
+    private static int sumPattern(Matcher pattern) {
         String customDelimiter = pattern.group(delimiternum);
         String[] patternList = pattern.group(textnum).split(customDelimiter);
         int total = 0;
@@ -72,7 +75,8 @@ public class StringAddCalculator {
         }
         return total;
     }
-    private static int sumAll(String text) {
+
+    private static int textIntoIntAndSumAll(String text) {
         String[] numbers = text.split(",|:");
         int total = 0;
         for (String number : numbers) {
